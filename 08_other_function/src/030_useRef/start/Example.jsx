8ffe8335c2bddfcdc0ed40045ee254d74cea5697
iyuta1124 <iyuta1124@gmail.com>
2024-10-05 14:32:03 +0900

    07 途中

--- 08_other_function/src/030_useRef/start/Example.jsx
+++ 08_other_function/src/030_useRef/start/Example.jsx
@@ -1,14 +1,48 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const Case1 = () => {
   const [value, setValue] = useState("");
+  const inputRef = useRef();
 
   return (
     <div>
       <h3>ユースケース1</h3>
-      <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
-      <button onClick>
+      <input
+        ref={inputRef}
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          inputRef.current.focus();
+        }}
+      >
         インプット要素をフォーカスする
+      </button>
+    </div>
+  );
+};
+
+const Case2 = () => {
+  const [playing, setPlaying] = useState(false);
+  const videoRef = useRef();
+  return (
+    <div>
+      <video ref={videoRef} style={{ maxWidth: "100%" }}>
+        <source src="./sample.mp4" />
+      </video>
+      <button
+        onClick={() => {
+          if (playing) {
+            videoRef.current.pause();
+          } else {
+            videoRef.current.play();
+          }
+          setPlaying((prev) => !prev);
+        }}
+      >
+        {playing ? "stop" : "play"}
       </button>
     </div>
   );
@@ -18,6 +52,7 @@ const Example = () => {
   return (
     <>
       <Case1 />
+      <Case2 />
     </>
   );
 };
